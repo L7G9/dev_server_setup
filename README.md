0ea13650b41c68df9cc05dfe45abda519ddb14c2
L7G9 <Lukewgregory@gmail.com>
2022-05-06 08:59:21 +0100

    Completed security file and updated setup file

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
-# dev_server_setup
-Scripts to setup a development environment on Ubuntu server
+# setup a develeopemnt enviroment on ubuntu server
+
+# how to use...
 
 # install git, clone public repo & run main script to install packages
-sudo get-apt update
-sudo get-apt upgrade
+git --version
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install git
 git clone https://github.com/l7g9/dev_server_setup
 cd dev_server_setup
 sudo bash dev_server_setup.sh
+
+# create a new sudo user
+adduser username
+usermod -aG sudo username
+su - username
+
+# setup git hub
+git config --global user.name "githunusername"
+git config --global user.email "myname@something.com"
--- security_main.sh
+++ security_main.sh
@@ -1,19 +1,14 @@
 #! /bin/bash
 
-bash security_non_root_sudo_user.sh
-echo Enter new user name...
-read username
-useradd $username
-passwd $username
-usermod -aG sudo $username
-
-# enble key based SSH authentication
-
-bash security_ufw.sh
+#bash security_ufw.sh
 ufw allow ssh
+#port used by router to forward ssh from outside network
+ufw allow 931
+#port used by jenkins
+ufw allow 8080
 ufw enable
 
-bash security_ssh.sh
+#bash security_ssh.sh
 # disable all shh forwarding
 echo "DisableForwarding yes" >> /etc/ssh/sshd_config.d/10-my-sshd-settings.conf
 # disable root login over ssh
@@ -21,11 +16,11 @@ echo "PermitRootLogin no" >> /etc/ssh/sshd_config.d/10-my-sshd-settings.conf
 # disable password based ssh authentication
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config.d/10-my-sshd-settings.conf
 
-bash security_rhosts.sh
+#bash security_rhosts.sh
 # ingore malicious rhosts file
 echo "IgnoreRhosts yes" >> /etc/ssh/sshd_config.d/10-my-sshd-settings.conf
 
-bash security_fail2ban.sh
+#bash security_fail2ban.sh
 apt-get install fail2ban net-tools -y
 # configure fail2ban to protect SSH
 echo "[ssh]
